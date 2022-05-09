06eb0bd64927a9e2b49dca47dd1c707f3449b7dd
Deyan Dobromirov <dvdvideo123@gmail.com>
2022-05-09 22:34:10 +0300

    Added: Primary laser refractor

---
+++ lua/entities/gmod_laser_refractor/init.lua
@@ -0,0 +1,56 @@
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
+include("shared.lua")
+
+resource.AddFile("materials/vgui/entities/gmod_laser_dimmer.vmt")
+
+function ENT:Initialize()
+  self:SetSolid(SOLID_VPHYSICS)
+  self:PhysicsInit(SOLID_VPHYSICS)
+  self:SetMoveType(MOVETYPE_VPHYSICS)
+
+  self:WireCreateInputs(
+    {"Normal", "VECTOR", "Dimmer surface normal"}
+  ):WireCreateOutputs(
+    {"Normal", "VECTOR", "Dimmer surface normal"},
+    {"Entity", "ENTITY", "Dimmer entity itself" }
+  )
+
+  self:SetBeamReplicate(false)
+  self:SetLinearMapping(false)
+
+  local phys = self:GetPhysicsObject()
+  if(LaserLib.IsValid(phys)) then
+    phys:Wake(); phys:SetMass(50)
+  end -- Apply decent mass
+
+  -- Setup default configuration
+  self:WireWrite("Entity", self)
+end
+
+function ENT:SpawnFunction(ply, tr)
+  if(not tr.Hit) then return end
+  local gen = LaserLib.GetTool()
+  local ang = LaserLib.GetAngleSF(ply)
+  local ent = ents.Create(LaserLib.GetClass(7, 1))
+  if(LaserLib.IsValid(ent)) then
+    LaserLib.SetMaterial(ent, LaserLib.GetMaterial(7))
+    LaserLib.SnapNormal(ent, tr.HitPos, tr.HitNormal, 90)
+    ent:SetAngles(ang) -- Appy angle after spawn
+    ent:SetCollisionGroup(COLLISION_GROUP_NONE)
+    ent:SetSolid(SOLID_VPHYSICS)
+    ent:SetMoveType(MOVETYPE_VPHYSICS)
+    ent:SetNotSolid(false)
+    ent:SetModel(LaserLib.GetModel(7))
+    ent:Spawn()
+    ent:SetCreator(ply)
+    ent:Activate()
+    ent:PhysWake()
+    LaserLib.SetProperties(ent, "metal")
+    LaserLib.SetPlayer(ent, ply)
+    ent:SetBeamTransform()
+    ply:AddCount(gen.."s", ent)
+    ply:AddCleanup(gen.."s", ent)
+    return ent
+  end
+end
---
+++ lua/entities/gmod_laser_refractor/shared.lua
@@ -0,0 +1,58 @@
+ENT.Type           = "anim"
+ENT.Category       = LaserLib.GetData("CATG")
+ENT.PrintName      = "Dimmer"
+ENT.Information    = ENT.Category.." "..ENT.PrintName
+if(WireLib) then
+  ENT.Base          = "base_wire_entity"
+  ENT.WireDebugName = ENT.Information
+else
+  ENT.Base          = "base_entity"
+end
+ENT.Editable       = true
+ENT.Author         = "DVD"
+ENT.Contact        = "dvdvideo123@gmail.com"
+ENT.Spawnable      = true
+ENT.AdminSpawnable = true
+ENT.RenderGroup    = RENDERGROUP_BOTH
+
+include(LaserLib.GetTool().."/wire_wrapper.lua")
+include(LaserLib.GetTool().."/editable_wrapper.lua")
+
+function ENT:SetupDataTables()
+  self:EditableSetVector("NormalLocal"  , "General") -- Used as forward
+  self:EditableSetBool  ("BeamReplicate", "General")
+  self:EditableSetBool  ("LinearMapping", "General")
+  LaserLib.Configure(self)
+end
+
+-- Override the beam transormation
+function ENT:SetBeamTransform()
+  local normal = Vector(0,0,1) -- Local surface direction
+  self:SetNormalLocal(normal)
+  return self
+end
+
+function ENT:GetHitNormal()
+  if(SERVER) then
+    local normal = self:WireRead("Normal", true)
+    if(normal) then normal:Normalize() else
+      normal = self:GetNormalLocal()
+    end -- Make sure length is one unit
+    self:SetNWVector("GetNormalLocal", normal)
+    self:WireWrite("Normal", normal)
+    return normal
+  else
+    local normal = self:GetNormalLocal()
+    return self:GetNWFloat("GetNormalLocal", normal)
+  end
+end
+
+function ENT:GetHitPower(normal, beam, trace, bmln)
+  local norm = Vector(normal)
+        norm:Rotate(self:GetAngles())
+  local dotm = LaserLib.GetData("DOTM")
+  local dotv = math.abs(norm:Dot(beam.VrDirect))
+  if(bmln) then dotv = 2 * math.asin(dotv) / math.pi end
+  local dott = math.abs(norm:Dot(trace.HitNormal))
+  return (dott > (1 - dotm)), dotv
+end
